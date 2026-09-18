8cab1da1b041cdde17fc20ee7e16ddc0eea44b9f
aliridowan <aliridowan.dev@gmail.com>
2026-09-19 00:04:24 +0600

    fixed theme toggle and global theme

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import Link from "next/link";
 import { getServerSession } from "../../helpers/get-servesession";
 import { redirect } from "next/navigation";
+import { ThemeToggle } from "../../components/ThemeToggle";
 
 export default async function AuthLayout({ children }: { children: ReactNode }) {
 
@@ -9,14 +10,15 @@ export default async function AuthLayout({ children }: { children: ReactNode }) 
   if (session?.user) redirect('/')
 
   return (
-    <div className="relative min-h-screen bg-[#0b0f19]">
-      <header className="absolute top-0 left-0 w-full p-6 z-50">
+    <div className="relative min-h-screen background">
+      <header className="flex justify-between top-0 left-0 w-full p-6 z-50">
         <Link href="/" className="inline-block">
-          <h1 className="text-2xl font-bold tracking-wider text-white">
+          <h1 className="text-2xl font-bold tracking-wider text-primary">
             G<span className="hidden sm:inline">ADGET</span>
             <span className="text-blue-500">B<span className="hidden sm:inline">ROO</span></span>
           </h1>
         </Link>
+        <ThemeToggle />
       </header>
       {children}
     </div>
